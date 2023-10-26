692093a455cd5ce713a9929f07a779c3c9329ab4
Devarasen <devarasen@gmail.com>
2023-10-26 19:44:02 +1100

    main.jsx setup - DS

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,36 +1,23 @@
-import React from 'react';
-import { Routes, Route } from 'react-router-dom';
-import Header from './pages/Header';
-import LandingPage from './pages/LandingPage';
-import Dashboard from './pages/Dashboard';
-import CommunityBoard from './pages/CommunityBoard';
-import Footer from './pages/Footer';
+import './App.css';
+import { Outlet } from 'react-router-dom';
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
+
+import Header from './pages/Header'
+
+const client = new ApolloClient({
+  uri: '/graphql',
+  cache: new InMemoryCache(),
+});
 
 function App() {
-  const isAuthenticated = false; // Replace with actual authentication check
-
   return (
-    <div className="app">
+    <ApolloProvider client={client}>
       <Header />
-      <Routes>
-        <Route path="/" element={<LandingPage />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/community" element={<CommunityBoard />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-      <Footer />
-    </div>
-  );
-}
-
-function NotFound() {
-  return (
-    <div>
-      <h1>404 - Not Found</h1>
-      <p>The page you're looking for does not exist.</p>
-    </div>
+      <Outlet />
+    </ApolloProvider>
   );
 }
 
 export default App;
+
 
