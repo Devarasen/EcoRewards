5803b5620abc5929ad352ce3c7ca85571a72f97c
Devarasen <devarasen@gmail.com>
2023-10-26 21:10:20 +1100

    style-setup-DS-2

--- client/src/pages/Footer.jsx
+++ client/src/pages/Footer.jsx
@@ -1,22 +1,17 @@
+import '../styles/Footer.css';
 
-
-const Footer = () => {
+function Footer () {
   return (
     <footer className="app-footer">
       <div className="footer-content">
         <div className="footer-section">
-          <h4>About EcoRewards</h4>
-          <p>
-            EcoRewards is a platform that encourages and rewards eco-friendly practices to make the world a better place.
-          </p>
+
         </div>
         <div className="footer-section">
           <h4>Contact Us</h4>
           <p>Email: contact@ecorewards.com</p>
         </div>
         <div className="footer-section">
-          <h4>Follow Us</h4>
-          <p>Stay connected on social media:</p>
           <ul className="social-links">
             <li><a href="#">Facebook</a></li>
             <li><a href="#">Twitter</a></li>
