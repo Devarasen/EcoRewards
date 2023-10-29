7a576d29a1a492afe820e4374ec59752dfc4cc3b
Marcelo Silva <marcelosilvaa92@gmail.com>
2023-10-29 18:00:25 +1100

    changes to headers, auth and dashboard

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,60 +1,48 @@
-import { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import '../styles/Dashboard.css';
-
+import AuthService from '../utils/auth'; 
 const Dashboard = () => {
-  // Replace this mock data with actual data retrieval logic
-  const currentUser = {
-    username: 'SampleUser',
-    ecoActions: [
-      {
-        action: 'Recycled plastic bottles',
-        greenCoinsEarned: 10,
-      },
-      {
-        action: 'Used energy-efficient bulbs',
-        greenCoinsEarned: 5,
-      },
-      // More eco-actions...
-    ],
-    totalGreenCoins: 15, // Calculate the total from eco-actions
-  };
+    const [currentUser, setCurrentUser] = useState({
+        username: AuthService.getUsername(),  
+        ecoActions: [],
+        totalGreenCoins: 0
+    });
+    const [donationAmount, setDonationAmount] = useState(0);
 
-  const [donationAmount, setDonationAmount] = useState(0);
+  
+    const handleDonation = () => {
+        if (donationAmount > 0) {
+            
+        }
+    };
 
-  const handleDonation = () => {
-    // Implement Stripe donation handling here
-    if (donationAmount > 0) {
-      // Call a function to initiate the Stripe donation process
-    }
-  };
-
-  return (
-    <div className="dashboard">
-      <h2>Dashboard</h2>
-      <h3>Welcome, {currentUser.username}!</h3>
-      <div className="eco-actions">
-        <h4>Your Eco-Actions:</h4>
-        <ul>
-          {currentUser.ecoActions.map((action, index) => (
-            <li key={index}>
-              {action.action} - {action.greenCoinsEarned} Green Coins
-            </li>
-          ))}
-        </ul>
-        <p>Total Green Coins Earned: {currentUser.totalGreenCoins}</p>
-      </div>
-      <div className="donation-section">
-        <h4>Make a Donation:</h4>
-        <input
-          type="number"
-          placeholder="Donation Amount (in Green Coins)"
-          value={donationAmount}
-          onChange={(e) => setDonationAmount(e.target.value)}
-        />
-        <button onClick={handleDonation}>Donate</button>
-      </div>
-    </div>
-  );
+    return (
+        <div className="dashboard">
+            <h2>Dashboard</h2>
+            <h3>Welcome, {currentUser.username}!</h3>
+            <div className="eco-actions">
+                <h4>Your Eco-Actions:</h4>
+                <ul>
+                    {currentUser.ecoActions.map((action, index) => (
+                        <li key={index}>
+                            {action.action} - {action.greenCoinsEarned} Green Coins
+                        </li>
+                    ))}
+                </ul>
+                <p>Total Green Coins Earned: {currentUser.totalGreenCoins}</p>
+            </div>
+            <div className="donation-section">
+                <h4>Make a Donation:</h4>
+                <input
+                    type="number"
+                    placeholder="Donation Amount (in Green Coins)"
+                    value={donationAmount}
+                    onChange={(e) => setDonationAmount(e.target.value)}
+                />
+                <button onClick={handleDonation}>Donate</button>
+            </div>
+        </div>
+    );
 };
 
 export default Dashboard;
