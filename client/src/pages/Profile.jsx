30290e829ff7e4e55bba45efd928f6f9aa8a9f0b
Devarasen <devarasen@gmail.com>
2023-10-30 18:24:35 +1100

    bug fix

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import '../styles/Profile.css';
 import AuthService from '../utils/auth'; 
-const Dashboard = () => {
+const Profile = () => {
     const [currentUser, ] = useState({
         username: AuthService.getUsername(),  
         ecoActions: [],
